beebce61e80ad5dfd40bbbaddaf7e553207b2ca2
Kiriesha <49344266+Kiriesha@users.noreply.github.com>
2022-10-07 10:04:06 +0300

    example Test JUnit

---
+++ src/test/java/guru/qa/ExampleJUnit5Test.java
@@ -0,0 +1,45 @@
+package guru.qa;
+
+import org.junit.jupiter.api.*;
+
+import java.lang.module.Configuration;
+
+public class ExampleJUnit5Test {
+
+    @BeforeAll
+    static void configure (){
+      //  Configuration.browser = "chrome";
+        System.out.println("this is @BeforeAll");
+    }
+
+    @AfterAll
+    static void after (){
+        System.out.println("this is @AfterAll");
+    }
+
+    @BeforeEach
+    void bef_each(){
+        System.out.println("this is @BeforeEach");
+    }
+
+    @BeforeEach
+    void After_each(){
+        System.out.println("this is @AfterEach");
+    }
+
+    @Test
+    void FTest(){
+        System.out.println("this is @FTest");
+    }
+
+    @Test
+    void STest(){
+        System.out.println("this is @STest");
+    }
+
+    @Test
+    void firstTest(){
+        //ассерты - утверждения
+        Assertions.assertTrue(3>2);
+    }
+}
